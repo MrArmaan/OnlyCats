26875e7bc1b59d8b7ff5921909adb7d451e617fa
quinn <williammsullivansabre@gmail.com>
2024-01-31 12:38:19 +0000

    API and cat cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./styles/App.css";
+import  CatGrid from "./services/CatApi.jsx"
+
 
 function App() {
   return (
     <>
       <h1>Welcome to the cat adoption website!</h1>
+    <CatGrid>
+
+    </CatGrid>
     </>
   );
 }
---
+++ src/services/CatAPI.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const CatGrid = () => {
+  const [imagesData, setImagesData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://api.thecatapi.com/v1/images/search?limit=20';
+      const api_key = 'live_5oFkLgqzJlEQoqfSM9wGAxXNFmRO04OisLkOKupqH5gc2PLAurQ9nUASoiraLDKK';
+
+      try {
+        const response = await fetch(url, {
+          headers: {
+            'x-api-key': api_key,
+          },
+        });
+        const data = await response.json();
+        setImagesData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  return (
+    <div className="maincontent">
+    <div className="imgrid" id="grid"> 
+      {imagesData.map((imageData, index) => (
+        <div className="col col-lg" key={index}>
+          <img src={imageData.url} alt={`Cat ${index}`} />
+        </div>
+      ))}
+    </div>
+    </div>
+  );
+};
+
+export default CatGrid;
