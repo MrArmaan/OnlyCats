9f000679ac7052c68ce326ca33b83b2637a24eda
quinn <williammsullivansabre@gmail.com>
2024-01-31 14:59:26 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./styles/App.css";
-import  CatGrid from "./services/CatApi.jsx"
+import  CatGrid from "./services/CatApi-CatGrid.jsx"
 
 
 function App() {
   return (
     <>
-      <h1>Welcome to the cat adoption website!</h1>
+      <h1>Welcome to OnlyCats!</h1>
     <CatGrid>
 
     </CatGrid>
--- src/services/CatApi-CatGrid.jsx
+++ src/services/CatApi-CatGrid.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
+
 const CatGrid = () => {
   const [imagesData, setImagesData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = 'https://api.thecatapi.com/v1/images/search?limit=20';
+      const url = 'https://api.thecatapi.com/v1/images/search?limit=21';
       const api_key = 'live_5oFkLgqzJlEQoqfSM9wGAxXNFmRO04OisLkOKupqH5gc2PLAurQ9nUASoiraLDKK';
 
       try {
