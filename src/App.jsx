fa9c43c39e07b8e5b8f991f7e2a4bd164b1637c2
Andrew Cookson <andrewcookson@email.com>
2024-02-01 21:53:47 +0000

    stable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home";
-import CatGrid from "./services/CatApi-CatGrid";
+import Catalog from "./pages/Catalog";
 import Guidelines from "./pages/Guidelines";
+import Checkout from "./pages/Checkout";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
 
@@ -12,8 +13,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Catalog" element={<CatGrid />} />
+          <Route path="/Catalog" element={<Catalog />} />
           <Route path="/Guidelines" element={<Guidelines />} />
+          <Route path="/Checkout" element={<Checkout />} />
         </Routes>
       </div>
     </Router>
